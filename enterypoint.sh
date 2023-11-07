b4155366e16ea86086d50e2323887e7e2e03d95f
ShadiAyoub <ayoubshadi75@gmail.com>
2023-11-07 08:05:16 +0300

    Update enterypoint.sh

--- enterypoint.sh
+++ enterypoint.sh
@@ -9,6 +9,12 @@ fi
 if [ -n "$NEW_VARIABLE" ]; then
   export MY_VARIABLE="$NEW_VARIABLE"
 fi
+# Set current time as variable
+timestamp=$(date +%s)
+hex_timestamp=$(printf "%x" $timestamp)
+prefixed_hex_timestamp="0x${hex_timestamp}"
+export TIMESTAMP=$prefixed_hex_timestamp
+echo $TIMESTAMP
 
 # Start the main application
 exec  npm start
