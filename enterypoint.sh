9174051bc0a010032da7f7efff7c34aa1110dce5
ShadiAyoub <ayoubshadi75@gmail.com>
2023-11-07 06:11:09 +0300

    Update enterypoint.sh
    Remove existing variables and load new ones upon each start.

--- enterypoint.sh
+++ enterypoint.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-exec npm start
+cd /genesis-tool/
+# Check for and load new variables or configuration files before starting the main application
+if [ -f ./config.json ]; then
+  source ./config.json
+fi
+
+# Check for and load new variables from environment variables
+if [ -n "$NEW_VARIABLE" ]; then
+  export MY_VARIABLE="$NEW_VARIABLE"
+fi
+
+# Start the main application
+exec  npm start
